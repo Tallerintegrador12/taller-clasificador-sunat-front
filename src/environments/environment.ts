136d5b2a7b30ebb91128c924d07b0f2875a6e62b
Tallerintegrador12 <tallerintegrador820@gmail.com>
2025-06-23 17:02:23 -0500

    fix: Restaurar URL de API para Azure deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://sunatapi-arcehmesgqb2f8en.brazilsouth-01.azurewebsites.net/api',
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'https://sunatapi-arcehmesgqb2f8en.brazilsouth-01.azurewebsites.net/api',
+  //apiUrl: 'http://localhost:8080/api',
   featureXEnabled: true,
   logging: true,
 };
